965df1f1cc07def883dc00e575acb132de703e4e
Tushar Priyadarshi <tusharreso333@gmail.com>
2026-01-06 18:28:30 +0530

    :wq ! insert :wq

---
+++ question2nd.cpp
@@ -0,0 +1,11 @@
+// You are given an integer array A of size N.
+
+// You are allowed to perform at most K swap operations.
+// In one swap operation, you may choose any two indices i and j and swap A[i] and A[j].
+
+// Note: Swapping the same pair of elements more than once has no effect (i.e., repeated swaps cancel out).
+
+// After performing at most K swaps, find the maximum possible sum of a contiguous subarray.
+
+// Return that maximum subarray sum.
+
